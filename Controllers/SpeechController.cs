2f43a279394411efcfd09fc528a61b857d9b4a08
Riyaz <Riyaz@DESKTOP-VMH5HO8>
2022-05-13 22:44:05 +0530

    Add project files.

---
+++ Controllers/SpeechController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeTextToSpeech.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpeechController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<SpeechController> _logger;
+
+        public SpeechController(ILogger<SpeechController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public JsonResult TextToSpeech()
+        {
+            return new JsonResult("Success");
+        }
+    }
+}
