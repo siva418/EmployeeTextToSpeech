50c4522e1748bd05dcf9a8486cf643681f706680
riyazpashamohammad <riyazpashamohammad@gmail.com>
2022-05-13 23:47:57 +0530

    Add text to speech from sdk

--- Controllers/SpeechController.cs
+++ Controllers/SpeechController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CognitiveServices.Speech;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -24,9 +25,45 @@ namespace EmployeeTextToSpeech.Controllers
         }
 
         [HttpGet]
-        public JsonResult TextToSpeech()
+        public async Task<JsonResult> TextToSpeech(string text)
         {
+            var speechConfig = SpeechConfig.FromSubscription(YourSubscriptionKey, YourServiceRegion);
+
+            // The language of the voice that speaks.
+            speechConfig.SpeechSynthesisVoiceName = "en-US-JennyNeural";
+
+            using (var speechSynthesizer = new SpeechSynthesizer(speechConfig))
+            {
+                var speechSynthesisResult = await speechSynthesizer.SpeakTextAsync(text);
+                OutputSpeechSynthesisResult(speechSynthesisResult, text);
+            }
             return new JsonResult("Success");
+        }
+
+        static string YourSubscriptionKey = "73d087ee47c54f608f7e5e00e5ff7151";
+        static string YourServiceRegion = "eastus";
+
+        public void OutputSpeechSynthesisResult(SpeechSynthesisResult speechSynthesisResult, string text)
+        {
+            switch (speechSynthesisResult.Reason)
+            {
+                case ResultReason.SynthesizingAudioCompleted:
+                    Console.WriteLine($"Speech synthesized for text: [{text}]");
+                    break;
+                case ResultReason.Canceled:
+                    var cancellation = SpeechSynthesisCancellationDetails.FromResult(speechSynthesisResult);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
+                        Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
